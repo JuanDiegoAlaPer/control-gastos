40ff6a44d49d0b7de944ace4d2231cbb9994ca43
JuanDiegoAlaPer <juandiegoap2000@gmail.com>
2023-05-14 22:49:42 -0500

    expense and income models made

---
+++ src/models/income.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+from src.database import db, ma
+
+class Income(db.Model):
+    code             = db.Column(db.String(10), primary_key=True)
+    balance          = db.Column(db.Double, nullable=False)
+    create_at        = db.Column(db.DateTime, default=datetime.now())
+    
+    account_code = db.Column(db.String(5), 
+                        db.ForeignKey('account.code',
+                                        onupdate="CASCADE",
+                                        ondelete="RESTRICT"), 
+                        nullable=False)
+    
+    
+class AccountSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        #fields = ()
+        model = Income
+        include_fk = True
+account_schema=AccountSchema()
+accounts_schema=AccountSchema(many=True)
